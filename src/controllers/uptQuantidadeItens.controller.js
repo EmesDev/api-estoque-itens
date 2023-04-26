7f00e9ef75c163ddf35632c685f3b7ec055a3ad1
Emerson Franco <contato.emersonfranco@gmail.com>
2023-04-26 16:22:52 -0300

    formatação

--- src/controllers/uptQuantidadeItens.controller.js
+++ src/controllers/uptQuantidadeItens.controller.js
@@ -2,22 +2,21 @@ const database = require('../database/connection');
 
 class UptQuantidadeItens {
     uptItem(req, res) {
-        let {quantidade, idItem} = req.body;
+        let { quantidade, idItem } = req.body;
 
         console.log(quantidade, idItem)
 
         database.raw('CALL pUptQuantidadeItem(?, ?)', [quantidade, idItem])
-            .then(data=>{
-                    console.log(data)
-                    res.json({
-                        message:"Quantidade de itens atualizada com sucesso", 
-                        status:200
-                    })
+            .then(data => {
+                console.log(data)
+                res.json({
+                    message: "Quantidade de itens atualizada com sucesso",
+                    status: 200
                 })
+            })
             .catch((erro) => {
                 console.error(erro);
             });
-
     }
 }
 
